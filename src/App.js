a96ffb5ce73919586820031392e176129bd91ab3
Tom Dalby <thomasjackdalby@gmail.com>
2019-12-05 19:17:03 +0000

    testing site

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import Leaderboard from './components/leaderboard';
+import NavBar from './components/navbar';
+import 'bootswatch/dist/darkly/bootstrap.min.css';
+import './App.css';
+
+class App extends Component {
+  state = {
+    "users" : []
+  }
+
+  componentDidMount() {
+    fetch('http://santa-shares.azurewebsites.net/api/users')
+    .then(res => res.json())
+    .then(data => {
+      data.sort(function(a, b) { return a.balance - b.balance; })
+      this.setState({ users: data })
+    })
+    .catch(console.log)
+  }
+
+  render () {
+    return (
+      <div>
+        <NavBar />
+        <Leaderboard users={this.state.users} />
+      </div>
+    )
+  }
+}
+
+export default App;
